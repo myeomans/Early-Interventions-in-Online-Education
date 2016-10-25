8df8130317c7fe74e38582c23e5b628c7b1d9ac4
myeomans <mk.yeomans@gmail.com>
2016-10-25 10:17:02 -0400

    model populations

--- regressions.R
+++ regressions.R
@@ -1,13 +1,18 @@
 require(lme4)
 
 test.data<-data[(data$treated==1)&(data$start_day<32),]
+fluent<-(test.data$fluency==5)
+low.dev<-(test.data$HDI<0.7)
 
 models<-list()
 
-models[["affirm"]]<-lmer(certified~affirm,data=test.data)
-models[["plans"]]<-lmer(certified~plans,data=test.data)
-models[["shortlong"]]<-lmer(certified~affirm*short.plans,data=test.data[test.data$plans==1,])
+# Full Sample
+models[["affirm"]]<-lmer(certified~affirm+(1|course),family="binomial",data=test.data)
+models[["affirm"]]<-lmer(certified~affirm+(1|course),family="binomial",data=test.data[low.dev,])
+# All Students
+models[["plans"]]<-lmer(certified~plans+(1|course),family="binomial",data=test.data[fluent,])
+models[["short.plans"]]<-lmer(certified~short.plans+(1|course),family="binomial",data=test.data[fluent,])
+models[["long.plans"]]<-lmer(certified~long.plans+(1|course),family="binomial",data=test.data[fluent,])
+models[["shortlong"]]<-lmer(certified~short.plans+(1|course),family="binomial",data=test.data[fluent&(test.data$plans==1),])
 
-models[["all"]]<-lmer(certified~affirm*plans,data=test.data)
-
-models[["all"]]<-lmer(certified~affirm*plans,data=test.data)
+models[["all"]]<-lmer(certified~affirm*(short.plans+long.plans)+(1|course),family="binomial",data=test.data)
