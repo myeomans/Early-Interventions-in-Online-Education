839244c4bbc9d1c2fa866d9142e33c8e58c8e370
Rene Kizilcec <rkxpost@gmail.com>
2017-01-18 11:23:49 -0800

    Cleaning up prereg analysis

--- prereg_no_ex.R
+++ prereg_no_ex.R
@@ -14,54 +14,57 @@ load("simdat.rda")
 # Sample Selection
 #######################################################
 #
-#  Survey software records every student who started a pre-course survey. 
-#  We impose a set of exclusion criteria to define our sample of interest.
-# 
-#  1. Student must have completed enough of the survey to be exposed to randomized condition
-data$exposed.to.treat<-1*(data$webservice_call_complete == 1)&(!is.na(data$affirm))&(!is.na(data$plans))
-#  2. Student must have started the course in the first 14 days
-data$start.day<-(data$first_activity_timestamp-data$course_start_timestamp)/(60*60*24)
-#  3. Student must have started the survey within the first hour of entering the course
-data$survey.delay<-(data$survey_timestamp-data$first_activity_timestamp)/(60*60) 
+#  Survey software records all learners who started the course survey.
+#  We impose a set of exclusion criteria to define our sample of interest:
+#  a. The learner must have completed enough of the survey to be randomized and exposed to condition
+#  b. The learner must have started the course in the first 14 days
+#  c. The learner must have started the survey within the first hour of entering the course
 
-data$itt.sample<-1*((data$exposed.to.treat==1)&(data$start.day<15)&(data$survey.delay<1))
+data = mutate(data,
+  exposed.to.treat = (webservice_call_complete == 1) & (!is.na(affirm)) & (!is.na(plans)), # a.
+  start.day = (first_activity_timestamp - course_start_timestamp) / (60*60*24), # b.
+  survey.delay = (survey_timestamp - first_activity_timestamp) / (60*60), # c.
+  itt.sample = exposed.to.treat & (start.day < 15) & (survey.delay < 1)
+)
 
-samples<-list()
-samples[["baseline"]] = (data$itt.sample==1)
+samples = list()
+samples[["baseline"]] = data$itt.sample
+
 #######################################################
-#### Statistical Model ####
+#### Stratified and Nested Design ####
 #######################################################
 #
 # The data are collected from a series of treatments embedded in a single procedure that was 
 # replicated in many courses, across several schools. Additionally, assignment to treatment
 # was stratified across four covariates collected early in the survey. 
-# 
+#
+# Stratification variables: intent_assess, hours, crs_finish, educ
+# Stratified: strata_intent_assess (2 levels), strata_hours (2), strata_crs_finish (3), strata_educ (3)
+# Strata encoding variables: strata {strata_intent_assess, strata_hours, strata_crs_finish, strata_educ}
+# Additional nesting variables: school, course
+#
 # In every model we control for strata covariates; and nesting within strata, course and school
-course.strata<-" + intent_assess + hours + crs_finish + educ + (1 | school/course) + (1 | strata)"
-#
-#### Variable Names ####
-# Stratification: strata {strata_intent_assess, strata_hours, strata_crs_finish, strata_educ}
-# Additional Nesting: school, course
-### Regression Models ##
-# only estimates the background covariates/nesting
-models<-list()
-models[["baseline"]] = "1"
-#
+course.strata = " + intent_assess + hours + crs_finish + educ + (1 | school/course) + (1 | strata)"
+
 #######################################################
 #### Randomized Treatments ####
 #######################################################
 #
-# Students were randomized into cells in a 2x3 between-subjects design.
+# Learners were randomized into cells in a 2x3 between-subjects design.
 #
-# Factor 1: Affirmation    (affirmed / control)
+# Factor 1: Affirmation    (affirm / control)
 # Factor 2: Plan-Making    (long plans / short plans / control)
 #
 # Randomization was done within strata
 # {strata_intent_assess, strata_hours, strata_crs_finish, strata_educ}
 #
 #### Variable Names ####
-# Condition Indicators: affirm, plans_long, plans_short
-### Regression Models ##
+# Binary condition indicators: affirm, plans_long, plans_short
+
+### Regression Models ###
+models = list()
+# only estimates the background covariates/nesting
+models[["baseline"]] = "1"
 # simple treatment effects
 models[["simple"]] = "affirm * (plans_long + plans_short) " 
 
@@ -81,20 +84,20 @@ models[["affirm.lang"]] = " affirm * (HDI + scale(fluent)) + (plans_long + plans
 models[["affirm.sex"]] = " affirm * sex + (plans_long + plans_short) "
 
 # Affirmation: effect for low SES (based on parental education)
-models[["affirm.sex"]]<-" affirm * scale(10-educ_parents) + (plans_long + plans_short)"
+models[["affirm.sex"]] = " affirm * scale(10-educ_parents) + (plans_long + plans_short)"
 
 # Affirmation: effect for US racial-ethnic minorities (majority = white/asian/non-hispanic)
 models[["affirm.minority"]] = " affirm * I(us_race%in%c(1,4) & us_ethnicity==1) + (plans_long + plans_short)"
 
-
 #######################################################
 ### Planned analyses for Plan-Making ###
 #######################################################
-data$plans_any<-1*(data$plans>0)
-models[["plan.original"]]<-"affirm + (plans_long + plans_short)"  
-models[["plan.vs.plan"]]<-"affirm + (plans_any + plans_long)"  
-samples[["plan.original"]]<- samples[["baseline"]]&(data$is_fluent==1)&(data$intent_assess==4)
-samples[["plan.vs.plan"]]<-samples[["plan.original"]]
+data$plans_any = as.numeric(data$plans > 0)
+
+models[["plan.original"]] = "affirm + (plans_long + plans_short)"  
+models[["plan.vs.plan"]] = "affirm + (plans_any + plans_long)"
+samples[["plan.original"]] = samples[["baseline"]] & (data$is_fluent==1) & (data$intent_assess==4)
+samples[["plan.vs.plan"]] = samples[["plan.original"]]
 
 
 #######################################################
@@ -104,29 +107,45 @@ samples[["plan.vs.plan"]]<-samples[["plan.original"]]
 # Percentage Outcomes: course_progress, likely_complete_1
 
 
-
+#######################################################
+####  Generalized Model Fitting Function #### 
+#######################################################
+fit_model = function(model.name, model.outcome) {
+  # Select relevant sample for model
+  if (model.name %in% names(samples)){
+    model.data = data[samples[[model.name]],]
+  } else {
+    model.data = data[samples[["baseline"]],]
+  }
+  
+  # Set Y to be the outcome of interest
+  model.data$Y = unlist(model.data[,model.outcome])
+  
+  # Estimate model parameters
+  if (mean(model.data$Y %in% (0:1)) == 1){
+    # for binary Y
+    model.fit = glmer(formula(paste("Y ~ ", models[[model.name]], course.strata)),
+                      data = model.data,
+                      family = "binomial",
+                      nAGQ = 0, 
+                      control = glmerControl(optimizer = "nloptwrap"))
+    
+  } else {
+    # for non-binary Y
+    model.fit = lmer(formula(paste("Y ~ ", models[[model.name]], course.strata)), 
+                    data = model.data)
+  }
+  
+  summary(model.fit)
+}
 
 #######################################################
-# Fitting the model
+#### Fitting the model #### 
 #######################################################
-model.outcome<-"cert_basic"
-model.name<-"simple"
+
+fit_model(model.name = "simple", model.outcome = "cert_basic")
+
+
+
 
 ##############################
-if(model.name%in%names(samples)){
-  model.data<-data[samples[[model.name]],]
-}else{model.data<-data[samples[["baseline"]],]}
-
-model.data$Y<-model.data[,model.outcome]
-if(mean(data$Y%in%(0:1))==1){
-  fit.glmer = glmer(formula(paste("Y ~ ", models[[model.name]], course.strata)),
-                    data = model.data,
-                    family = "binomial",
-                    nAGQ = 0, 
-                    control = glmerControl(optimizer = "nloptwrap"))
-  summary(fit.glmer)
-}else{
-  fit.lmer = lmer(formula(paste("Y ~ ", models[[model.name]], course.strata)), 
-                  data = model.data)
-  summary(fit.lmer)
-}
